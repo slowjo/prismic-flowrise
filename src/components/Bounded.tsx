26c7506f23d4deb8d7bf811915773add9f2f1b52
slowjo <fabiansen.s83@gmail.com>
2023-10-29 15:19:54 +0200

    feat: Add variation to Hero slice

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -9,7 +9,7 @@ type BoundedProps = {
 export default function Bounded({ as : Comp = "section", className, children, ...restProps } : BoundedProps) {
     return (
          <Comp className={clsx("px-4 py-10 md:py-14 md:px-6 lg:py-16", className)} {...restProps}>
-            <div className="mx-auto w-full max-w-3xl">
+            <div className="mx-auto w-full max-w-6xl">
                 {children}
             </div>
          </Comp>
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -13,7 +13,7 @@ const components: JSXMapSerializer = {
     </Heading>
   ),
   paragraph: ({ children }) => (
-    <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-xl">{children}</p>
+    <p className="text-2xl font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-xl">{children}</p>
   ),
 }
 
@@ -27,17 +27,38 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
+    <>
+    {slice.variation === "default" && (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     > 
         <div className="grid grid-cols-1 place-items-center text-center">
+          
           <PrismicRichText field={slice.primary.heading} components={components} />
           <PrismicRichText field={slice.primary.body} components={components} />
           <Button field={slice.primary.button_link} className="mb-8 md:mb-10">{slice.primary.button_text}</Button>
           <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full" />
         </div>
     </Bounded>
+    )}
+
+    {slice.variation === "horizontal" && (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    > 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center">
+          <div className="grid grid-rows-[1fr,auto,auto] h-fit">
+            <PrismicRichText field={slice.primary.heading} components={components} />
+            <PrismicRichText field={slice.primary.body} components={components} />
+            <Button field={slice.primary.button_link} className="mb-8 md:mb-10">{slice.primary.button_text}</Button>
+          </div>
+          <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full" />
+        </div>
+    </Bounded>
+    )}
+    </>
   );
 };
 
